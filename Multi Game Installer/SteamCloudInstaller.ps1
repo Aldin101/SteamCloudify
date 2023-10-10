3f8c90cd2e572851242fc6ae0055f38fb31250ba
Aldin <jindalpetersonab@outlook.com>
2023-10-09 19:39:00 -0700

    Online installer base is finished

--- Multi Game Installer/SteamCloudInstaller.ps1
+++ Multi Game Installer/SteamCloudInstaller.ps1
@@ -7,6 +7,16 @@ if ($database -eq $null) {
     exit
 }
 
+echo "Welcome to Steam Cloud setup"
+echo "Here are some things to know:"
+echo "This tool is not inteded as a backup, it is only inteded to sync your saves between computers, please us other tools for" "backups such as GameSaveManager"
+echo "Your saves will only be synced with other computers that have this tool installed"
+echo "When you install on another computer you will have the choice to download your saves from the cloud or upload your saves" "to the cloud, once you choose to override saves on a computer or the cloud you will not be able to recover the" "overritten saves"
+echo "You can disable Steam Cloud on this computer for any game by using this setup tool again"
+echo "Steam Deck (and other non-windows devices) are unsupported at this time"
+timeout -1
+cls
+
 $steamPath = (Get-ItemProperty -path 'HKCU:\SOFTWARE\Valve\Steam').steamPath
 $ids = Get-ChildItem -Path "$steamPath\userdata\"
 $found = $false
@@ -33,7 +43,7 @@ if ($steamid -eq $null) {
 $i=1
 $options = [System.Collections.ArrayList](@())
 foreach ($game in $database.games) {
-    if (test-path "$steamPath\userdata\$steamid\config\librarycache\$($game.steamID).json") {
+    if (test-path "$steamPath\steamapps\appmanifest_$($game.steamID).acf") {
         echo "[$i] $($game.name)"
         $options.add($game) | out-null
         ++$i
@@ -45,7 +55,18 @@ $choice = Read-Host "What game would you like to enable Steam Cloud for?"
 if ($choice -eq $i) {
     echo "Making support for a new game is easy! If the game runs a common game engine like Unity or Unreal" "there are ready to use templates already availble!"
     echo "You can find instructions to do so here: TEMP URL"
+    echo "Press any key to open the URL and exit"
+    timeout -1 | Out-Null
+    exit
+}
+
+if ([int]$choice -gt $i-1 -or [int]$choice -lt 1) {
+    echo "That is not a valid game"
     echo "Press any key to exit"
     timeout -1 | Out-Null
     exit
 }
+
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($options[$choice-1].installer.Script)) | iex
+
+echo "Steam Cloud for $($options[$choice-1].name) has been enabled! Remeber to install on other computers" "to sync your saves"
