2a19e3b400dfced49c1b3c84ddc2e3cbe5671417
Aldin101 <jindalpetersonab@outlook.com>
2023-10-23 14:29:41 -0700

    explain how the path is relative to the exe

--- .Templates/Saves in game folder/Background.ps1
+++ .Templates/Saves in game folder/Background.ps1
@@ -4,9 +4,15 @@ $gameName = "[INSERT GAME NAME]" # name of the game
 $steamAppID = "[INSERT STEAM APP ID]" # you can find this on https://steamdb.info, it should be structured like this, "NUMBER"
 $gameExecutableName = "[INSERT EXECUTABLE NAME]" # executable name should be structured, "GAME NAME.exe"
 $gameFolderName = "[INSERT FOLDER NAME]" # install folder should be structured like this, "GAME FOLDER NAME" just give the folder name
-$gameSaveFolder = '$gameSaveFolder = "$gamepath\[INSERT SAVE LOCATION]"' # the folder where saves are located, if the game does not store save files in a folder comment this out-
-# -If the game does it should be structured like this "$gamepath\location\to\saves". 
-# If you do not know where save files are use option 5 in the build tool (open Build.ps1 and press the play button in the top right corner)
+$gameSaveFolder = '$gameSaveFolder = "$gamepath\[INSERT SAVE LOCATION]"' # the folder where saves are located, if the game does not store save files- 
+# -in a folder comment this out.
+# If the game does it should be structured like this '$gameSaveFolder = "$gamepath\location\to\saves"'. This value uses the game executable location as a-
+# -starting point, not the root of the folder so if the game executable is located in "C:\Program Files (x86)\Steam\steamapps\common\Game Name\Game.exe" and-
+# -the save files are located in "C:\Program Files (x86)\Steam\steamapps\common\Game Name\Save Files" the value should be '$gameSaveFolder = "$gamepath\Save Files"'
+# but if the game executable is located in "C:\Program Files (x86)\Steam\steamapps\common\Game Name\win10\binaries\Game.exe" and the save files are located in-
+# "C:\Program Files (x86)\Steam\steamapps\common\Game Name\Save Files" the value should be '$gameSaveFolder = "$gamepath\..\..\Save Files"'
+# the ".." means to go back one folder, so make sure that you are going back the correct amount of folders if the game exe is not stored in the root of the game-
+# -folder. If you need help with this contact me on Discord by sending a friend request to aldin101
 $gameSaveExtensions = "[INSERT SAVE FILE EXTENSIONS]" # the game save folder sometimes contains information other than just game saves, and some-
 # -files should not be uploaded to Steam Cloud. If there is one extension format it like this ".[EXTENSION]". If there are more that one format it like this
 # "[EXTENSION1]", "[EXTENSION2]", "[EXTENSION3]"
@@ -14,7 +20,6 @@ $gameRegistryEntries = "[INSERT REGISTRY LOCATION]" # the location where registr
 # -comment this out by simply putting a "#" before the "$". If the game does it should be structured like this "HKCU\SOFTWARE\[COMPANY NAME]\[GAME NAME]"-
 # -(this is where most games store entires).
 # Game specific end------------------------------------------------------------------------------------------------------------------------------
-
 
 $cloudName = "$gameName Steam Cloud"
 $databaseURL = "https://aldin101.github.io/Steam-Cloud/$($gameName.Replace(' ', '%20'))/$($gameName.Replace(' ', '%20')).json"
